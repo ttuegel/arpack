28b6f8c65733bd648568456dc02f701cb59451f2
Thomas Tuegel <ttuegel@gmail.com>
2015-12-22 14:37:01 -0600

    Update to hmatrix >= 0.16

--- test/Properties.hs
+++ test/Properties.hs
@@ -7,14 +7,13 @@ import Data.AEq
 import Data.Complex
 import Data.List (sort, sortBy)
 import Data.Ord (comparing)
-import qualified Data.Packed as HMatrix
-import qualified Data.Vector.Unboxed as Unboxed
+import qualified Data.Vector.Unboxed as VU
 import Numeric.Arpack
 import qualified Numeric.LinearAlgebra as HMatrix
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty
 
-type Mat t = Unboxed.Vector (Int, Int, t)
+type Mat t = VU.Vector (Int, Int, t)
 
 properties =
   testGroup "QuickCheck properties"
@@ -27,7 +26,7 @@ properties =
 deig :: Mat Double -> [Double]
 deig mat = map realPart
            $ zeig
-           $ Unboxed.map (\(i, j, x) -> (i, j, x :+ 0)) mat
+           $ VU.map (\(i, j, x) -> (i, j, x :+ 0)) mat
 
 zeig :: Mat (Complex Double) -> [Complex Double]
 zeig mat = take (dim mat - 2)
@@ -37,31 +36,31 @@ zeig mat = take (dim mat - 2)
            $ densify mat
 
 deigs :: Mat Double -> [Double]
-deigs mat = Unboxed.toList $ eigenvalues (opts mat) mat (dim mat)
+deigs mat = VU.toList $ eigenvalues (opts mat) mat (dim mat)
 
 zeigs :: Mat (Complex Double) -> [Complex Double]
-zeigs mat = Unboxed.toList $ eigenvalues (opts mat) mat (dim mat)
+zeigs mat = VU.toList $ eigenvalues (opts mat) mat (dim mat)
 
-opts :: Unboxed.Unbox t => Unboxed.Vector (Int, Int, t) -> Options t
+opts :: VU.Unbox t => VU.Vector (Int, Int, t) -> Options t
 opts mat = Options { which = (Smallest, Real)
                    , number = dim mat - 2
                    , maxIterations = Nothing
                    }
 
-densify :: (HMatrix.Element t, Unboxed.Unbox t) => Mat t -> HMatrix.Matrix t
+densify :: (HMatrix.Element t, VU.Unbox t) => Mat t -> HMatrix.Matrix t
 densify mat = HMatrix.fromRows $ map selectRow [0..(dim mat - 1)]
   where
     selectRow i = HMatrix.fromList
-                  $ Unboxed.toList
-                  $ Unboxed.map (\(_, _, x) -> x)
-                  $ Unboxed.filter (\(j, _, _) -> i == j) mat
+                  $ VU.toList
+                  $ VU.map (\(_, _, x) -> x)
+                  $ VU.filter (\(j, _, _) -> i == j) mat
 
-dim :: (Unboxed.Unbox t) => Unboxed.Vector (Int, Int, t) -> Int
-dim mat = let (_, cols, _) = Unboxed.unzip3 mat in (Unboxed.maximum cols) + 1
+dim :: (VU.Unbox t) => VU.Vector (Int, Int, t) -> Int
+dim mat = let (_, cols, _) = VU.unzip3 mat in (VU.maximum cols) + 1
 
-instance (QC.Arbitrary t, Unboxed.Unbox t) => QC.Arbitrary (Unboxed.Vector (Int, Int, t)) where
+instance (QC.Arbitrary t, VU.Unbox t) => QC.Arbitrary (VU.Vector (Int, Int, t)) where
   arbitrary = do
     n <- fmap abs arbitrarySizedIntegral `suchThat` (> 2)
-    xs <- fmap Unboxed.fromList $ vector (n * n)
-    let ixs = Unboxed.fromList $ sort [(i, j) | i <- [0..(n - 1)], j <- [0..(n - 1)]]
-    return $ Unboxed.zipWith (\(i, j) x -> (i, j, x)) ixs xs
+    xs <- fmap VU.fromList $ vector (n * n)
+    let ixs = VU.fromList $ sort [(i, j) | i <- [0..(n - 1)], j <- [0..(n - 1)]]
+    return $ VU.zipWith (\(i, j) x -> (i, j, x)) ixs xs
